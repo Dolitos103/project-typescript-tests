971bba6996dcd9fe79c59628d140e72c4b7f454e
douglas.santos <douglas.santos@nava.com.br>
2023-02-02 23:03:10 -0300

    Efetuado criação de novos arquivos

---
+++ Tipos/tipo.ts
@@ -0,0 +1,14 @@
+// string
+let nome = 'João';
+console.log(nome);
+//nome = 28
+
+// numbers
+let idade = 27;
+idade = 27.5;
+console.log(idade);
+
+//boolean
+let possuiHobbies = false;
+//possuiHobbies = 1;
+console.log(possuiHobbies);
